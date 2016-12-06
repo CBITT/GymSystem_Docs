e6c70189e2fe5ba9598df709d9f297fd9fad7eee
Neil Sankar <njfs7695@gmail.com>
2016-12-06 02:13:20 +0000

    Tester to check if room capacity random is working

---
+++ RoomsTest.java
@@ -0,0 +1,17 @@
+
+import java.util.ArrayList;
+import java.io.IOException;
+
+
+
+public class RoomsTest{
+    public static void main(String [] args){
+    
+    Rooms room = new Rooms();
+    
+    System.out.print(room);
+    
+
+  }
+    
+}
