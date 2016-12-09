33fa4010314f3a0c6cc08ac86a02e527535c92ec
cbitt <csbango@yahoo.com>
2016-12-09 15:24:26 +0000

    Created abstract class. > BookingGeneral generateReferenceNum() getReferenceNum()

---
+++ BookingGeneral.java
@@ -0,0 +1,11 @@
+/**
+ * Created by BangoCs on 12/9/2016.
+ */
+public abstract class BookingGeneral {
+
+    int referenceNum;
+
+    abstract void generateRefNum();
+
+    abstract int getReferenceNum();
+}
